50abcc39f72289cb341bd8272325c6fbcbd41353
Christian M. Carrasco <112918088+Daraash@users.noreply.github.com>
2025-11-18 11:26:15 -0500

    Create daraash.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/daraash.py
@@ -0,0 +1,30 @@
+#Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+#https://www.python.org/
+
+#Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+"""
+Esto también es 
+un comentario
+en varias líneas
+"""
+
+#Crea una variable (y una constante si el lenguaje lo soporta).
+
+my_variable = "Hola"
+my_variable = "Nuevo valor de la variable"
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+my_int = 123   #entero
+my_float = 4.5   #decimal
+my_bool = True   #booleano, puede ser verdadero o falso (True or False)
+my_bool = False
+my_string = "¡Hola, [Python]!"   #cadena
+
+print(type(my_bool))
+
+#Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print(my_string)
